d2057c494ac49eae31f6c80fc2c0086e3e11fa62
Mahdi Teymoori Anar <mahditeymoorianar@gmail.com>
2025-11-08 15:09:20 +0330

    add note activity

---
+++ app/src/main/java/com/teymoorianar/amnnote/ui/note/NoteActivity.kt
@@ -0,0 +1,47 @@
+package com.teymoorianar.amnnote.ui.note
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.teymoorianar.amnnote.ui.theme.AmnNoteTheme
+
+class NoteActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AmnNoteTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AmnNoteTheme {
+        Greeting("Android")
+    }
+}

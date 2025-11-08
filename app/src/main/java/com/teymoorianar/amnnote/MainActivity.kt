24e793ace4cb704b2cec62e638459d074255248a
Mahdi Teymoori Anar <mahditeymoorianar@gmail.com>
2025-11-08 15:08:50 +0330

    add preview for the main activity

--- app/src/main/java/com/teymoorianar/amnnote/MainActivity.kt
+++ app/src/main/java/com/teymoorianar/amnnote/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.teymoorianar.amnnote
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateContentSize
@@ -45,11 +47,14 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.getString
 import androidx.core.graphics.scaleMatrix
+import com.teymoorianar.amnnote.ui.note_activity.NoteActivity
 import com.teymoorianar.amnnote.ui.theme.AmnNoteTheme
 import androidx.compose.material3.MaterialTheme as Material3Theme
 
@@ -59,7 +64,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AmnNoteTheme {
-                CurvedBottomBarScreen(this@MainActivity)
+                CurvedBottomBarScreen()
             }
         }
     }
@@ -67,7 +72,7 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CurvedBottomBarScreen(context: Context) {
+fun CurvedBottomBarScreen() {
     val backgroundColor = Material3Theme.colorScheme.background
 
     Scaffold(
@@ -142,8 +147,15 @@ fun CurvedBottomBar() {
 @Composable
 fun CenterFab() {
     var expanded by remember { mutableStateOf(false) }
+    val context = LocalContext.current
     FloatingActionButton(
-        onClick = { /* TODO: handle FAB click */ },
+        onClick = {
+            val intent = Intent(
+                context,
+                NoteActivity::class.java
+            )
+            context.startActivity(intent)
+        },
         shape = CircleShape,
         backgroundColor = Material3Theme.colorScheme.primary,
         contentColor = Color.White,
@@ -151,9 +163,14 @@ fun CenterFab() {
         Icon(
             imageVector = Icons.Rounded.Create,
             contentDescription = "Add",
-            modifier = Modifier.size(30.dp, 30.dp)
-                .clickable { expanded = !expanded },
+            modifier = Modifier.size(30.dp, 30.dp),
         )
 
     }
+}
+
+@Preview
+@Composable
+fun preview() {
+    CurvedBottomBarScreen()
 }

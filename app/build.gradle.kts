abe10905dfe1b4b4b88b49411ffc8dc8b15cc0d5
mahditeymoorianar <79321972+mahditeymoorianar@users.noreply.github.com>
2025-11-15 16:57:48 +0330

    Add Material Components dependency for Material3 theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.preference.ktx)
+    implementation(libs.google.material)
 
     // Room dependencies (comment out if not using yet)
     implementation(libs.androidx.room.runtime)
